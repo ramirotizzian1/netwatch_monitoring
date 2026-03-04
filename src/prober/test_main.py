308deb24432f66e700dca852a1318950e25280a4
Ramiro Tizzian <ramirotizzian@gmail.com>
2026-03-03 21:48:18 -0300

    test: agrega prueba basica para desbloquear pytest

---
+++ src/prober/test_main.py
@@ -0,0 +1,8 @@
+# src/prober/test_main.py
+
+def test_ambiente_ok():
+    """
+    Un test básico para asegurar que pytest funcione
+    y el pipeline de CI/CD pueda continuar.
+    """
+    assert True == True
